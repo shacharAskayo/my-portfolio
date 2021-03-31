25560bb399ab3c47bc2a292ffe376a3d286fcc4a
Shachar Askayo <askayo122@gmail.com>
2021-03-31 17:39:37 +0300

    app upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,19 @@ import { ProjectsList } from '../cmps/ProjectsList';
 import { useEffect, useState } from 'react';
 import { ProjectsDynamicModal } from '../cmps/ProjectsDynamicModal';
 import { Logos } from '../cmps/Logos';
-import angular from 'assets/imgs/logos/angular-logo.png'
-import redux from 'assets/imgs/logos/redux-logo.png'
-import react from 'assets/imgs/logos/react-logo.png'
-import css from 'assets/imgs/logos/css-logo.png'
-import scss from 'assets/imgs/logos/scss-logo.png'
-import vue from 'assets/imgs/logos/vue-logo.png'
-import vueX from 'assets/imgs/logos/vueX-logo.png'
-import js from 'assets/imgs/logos/js-logo.png'
-import html from 'assets/imgs/logos/html-logo.png'
-import express from 'assets/imgs/logos/express-logo.png'
-import hooks from 'assets/imgs/logos/hooks-logo.png'
-import node from 'assets/imgs/logos/nodejs-logo.png'
-import mongo from 'assets/imgs/logos/mongo-screenshot.png'
+import angular from '../assets/imgs/logos/angular-logo.png'
+import redux from '../assets/imgs/logos/redux-logo.png'
+import react from '../assets/imgs/logos/react-logo.png'
+import css from '../assets/imgs/logos/css-logo.png'
+import scss from '../assets/imgs/logos/scss-logo.png'
+import vue from '../assets/imgs/logos/vue-logo.png'
+import vueX from '../assets/imgs/logos/vueX-logo.png'
+import js from '../assets/imgs/logos/js-logo.png'
+import html from '../assets/imgs/logos/html-logo.png'
+import express from '../assets/imgs/logos/express-logo.png'
+import hooks from '../assets/imgs/logos/hooks-logo.png'
+import node from '../assets/imgs/logos/nodejs-logo.png'
+import mongo from '../assets/imgs/logos/mongo-screenshot.png'
 
 
 
